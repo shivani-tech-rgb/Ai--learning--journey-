e38cd25f3ba6b3e59d32990a60ac4b2b9b9bec66
shivani-tech-rgb <upadhyayshivani999@gmail.com>
2026-04-03 10:20:45 +0530

    Create Ai_chatbot.py

---
+++ Ai_chatbot.py
@@ -0,0 +1,10 @@
+Simple AI Chatbot (Beginner Project)
+
+print("Hello! I am a simple AI chatbot.")
+name = input("What is your name? ")
+
+print("Nice to meet you,", name)
+
+question = input("Ask me something about AI: ")
+
+print("AI is the technology that allows machines to learn and make decisions like humans.")
