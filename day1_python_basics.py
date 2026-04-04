e0ec5990977b8488fb2a5977d93677f719186451
shivani-tech-rgb <upadhyayshivani999@gmail.com>
2026-04-04 12:56:42 +0530

    Create day1_python_basics.py

---
+++ day1_python_basics.py
@@ -0,0 +1,10 @@
+# Day 1 - Python Basics
+# Learning print() and input()
+
+print("Hello! I am learning Python.")
+
+name = input("Enter your name: ")
+print("Nice to meet you,", name)
+
+age = input("Enter your age: ")
+print("You are", age, "years old.")
