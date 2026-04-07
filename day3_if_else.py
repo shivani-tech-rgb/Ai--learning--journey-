ce37611b9b2ef98cca5b09160dafb0c024362549
shivani-tech-rgb <upadhyayshivani999@gmail.com>
2026-04-07 12:23:02 +0530

    Create day3_if_else.py

---
+++ day3_if_else.py
@@ -0,0 +1,9 @@
+Day 3 - If Else Condition
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+Commit message
